d28188238b0ed02109c318a1615fb88eebe85a2a
Vi PC <andreivierasu@gmail.com>
2021-11-25 21:04:50 +0200

    Working with strings

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -7,6 +9,41 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Andreyul Thau 1900");
+            string stringulMieu = "Hi";
+            string urmatorulString = "lume";
+
+            string a = "a";
+            string b = "b";
+            
+            int resultat = string.Compare(a, b);
+            // daca resultat e -1 atunci a e in partea stanga a lui b, daca e 1 atunci e in partea dreapta, daca e 0 atunci sunt egale
+            // string.compare are o a treia variabila care ignoreCase. By default e faulse, dar o putem seta pe true
+
+
+            //string concat
+            stringulMieu = stringulMieu + urmatorulString;
+            string theUltimateString = string.Concat(stringulMieu, urmatorulString);
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            stopWatch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                stringulMieu += i;
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);
+
+            Stopwatch stopWatchnou = Stopwatch.StartNew();
+            stopWatchnou.Start();
+            StringBuilder builder = new StringBuilder();
+            builder.Append(stringulMieu);
+            for (int i = 0; i < 100000; i++)
+            {
+                builder.Append(i);
+            }
+            stopWatchnou.Stop();
+            Console.WriteLine(stopWatchnou.Elapsed);
         }
     }
 }
